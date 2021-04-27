1a43184084f5bc96bb1371e677f5b9de4f7547db
Austin <austinlake04@gmail.com>
2021-04-26 22:58:51 -0700

    Spring 2 - Receipt View

---
+++ app/src/main/java/com/codepath/expensetracker/fragments/ReceiptsFragment.java
@@ -0,0 +1,102 @@
+package com.codepath.expensetracker.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.expensetracker.Receipt;
+import com.codepath.expensetracker.ReceiptsAdapter;
+import com.codepath.expensetracker.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ReceiptsFragment extends Fragment {
+
+    public static final String TAG = "ReceiptsFragment";
+    private RecyclerView rvReceipts;
+    protected ReceiptsAdapter adapter;
+    protected List<Receipt> allReceipts;
+    private SwipeRefreshLayout swipeContainer;
+
+    public ReceiptsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_receipts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvReceipts = view.findViewById(R.id.rvReceipts);
+
+        allReceipts = new ArrayList<>();
+        adapter = new ReceiptsAdapter(getContext(), allReceipts);
+
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i(TAG, "fetching new data!");
+                queryReceipts();
+            }
+        });
+
+        // Steps to use the recycler view:
+        // 0. create layout for one row in the list
+        // 1. create the adapter
+        // 2. create the data source
+        // 3. set the adapter on the recycler view
+        rvReceipts.setAdapter(adapter);
+        // 4. set the layout manager on the recycler view
+        rvReceipts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryReceipts();
+    }
+
+    protected void queryReceipts() {
+        ParseQuery<Receipt> query = ParseQuery.getQuery(Receipt.class);
+        query.include(Receipt.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Receipt.KEY_CREATED_KEY);
+        query.findInBackground(new FindCallback<Receipt>() {
+            @Override
+            public void done(List<Receipt> receipts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting receipts", e);
+                    return;
+                }
+                for (Receipt receipt : receipts) {
+                    Log.i(TAG, "Receipt: " + receipt.getDescription() + ", username: " + receipt.getUser().getUsername());
+                }
+                adapter.clear();
+                adapter.addAll(receipts);
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+}
